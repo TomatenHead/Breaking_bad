a93e2b610c199368f01a4854352a6298fe6700b1
Amin+Yanis <128040238+TomatenHead@users.noreply.github.com>
2025-03-07 13:18:26 +0100

    v0.3.1

--- bakeing-bread/animation_player1.1.3.gd
+++ bakeing-bread/animation_player1.1.3.gd
@@ -11,20 +11,9 @@ func _process(delta: float) -> void:
 	pass
 
 
-
-
-
-func _on_button_3_pressed() -> void:
-	get_tree().change_scene_to_file("res://levelfail1.1.1.1.tscn")
-
-
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://levelfail1.1.1.2.tscn")
-
-
-func _on_button_4_pressed() -> void:
+func _on_button_4_button_up() -> void:
 	get_tree().change_scene_to_file("res://Levelmenu1.tscn")
 
 
-func _on_button_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://level1.1.6.tscn")
+func _on_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://levelfail1.1.1.tscn")
