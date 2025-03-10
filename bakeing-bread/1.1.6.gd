d2597c38561bdd4d5af4cb67084f2aee47ad60bc
Amin+Yanis <128040238+TomatenHead@users.noreply.github.com>
2025-03-10 21:14:00 +0100

    v0.3.2

---
+++ bakeing-bread/1.1.6.gd
@@ -0,0 +1,13 @@
+extends AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	play("gun")
+	await get_tree().create_timer(5).timeout
+	get_tree().change_scene_to_file("res://level1.1.7.tscn") 
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ bakeing-bread/video_stream_player.gd
@@ -0,0 +1,11 @@
+extends VideoStreamPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	await get_tree().create_timer(15.0).timeout
+	get_tree().quit()
+	get_tree().change_scene_to_file("res://level1.1.8.tscn")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
